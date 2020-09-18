60063f3d3f411f9ef252c5a94b0d2ab651367ce7
PWuttam <mito.takuwan@hotmail.co.jp>
2020-09-18 19:09:26 +0545

    “text140_#12”

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
       <input type="text" name="personal_name"><br><br>
       投稿メッセージ：<br>
       <!-- textareaにmaxlength="140"を入れても文字数制限できる -->
-      <textarea name="contents" cols="40" rows="8"></textarea><br><br>
+      <textarea name="contents" cols="40" rows="8" maxlength="140"></textarea><br><br>
       <input type="submit" name="btn1" value="書き込む">
     </form>
 
